184da5d360de229dd6505705db7493bdd5f82a29
Suhaim <suhaim@userevents.com>
2013-09-21 21:59:31 -0300

    accepting arguments

--- getweather/getweather.py
+++ getweather/getweather.py
@@ -1,13 +1,13 @@
 import json
+import sys
 import urllib2
-weather = json.load(urllib2.urlopen("http://localhost:8080/?lat=-122.423&long=37.8267"))
+from sys import argv
 
+script, setPrecipIntensity = argv
+weather = json.load(urllib2.urlopen("http://localhost:8080/?lat=-122.423&long=37.8267"))
 precipIntensity = weather['precipIntensity']
 
-if precipIntensity == 0:
+if precipIntensity == (int(setPrecipIntensity)): 
   print("wake up for a dry run")
 else:
   print("sleep in as it is wet")
-
-#print weather['precipIntensity']
-
