b7179d95c0626a99f3becd812b3a4e6948e2b3dc
Nicolas Neu <nicolas.m.neu@gmail.com>
2013-09-22 13:55:40 -0300

    xxx

--- rasp/motionsens.py
+++ rasp/motionsens.py
@@ -6,11 +6,12 @@ import urllib2
 
 def get_filelist(mypath):
     onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+    return onlyfiles
 
 if __name__ == '__main__':
-    mypath = '/home/suhaim/.motion'
+    #mypath = '/home/suhaim/.motion'
     host = 'http://192.168.236.162:5000/'
-    #mypath = '/users/nico/__scratch'
+    mypath = '/users/nico/__scratch'
     onlyfiles = get_filelist(mypath)
     initial_no_of_files = len(onlyfiles)
     while True:
