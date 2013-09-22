462f235eacf440f739dc31da975d711399332ed7
Suhaim <suhaim@userevents.com>
2013-09-21 22:32:29 -0300

    new url

--- getweather/getweather.py
+++ getweather/getweather.py
@@ -4,7 +4,7 @@ import urllib2
 from sys import argv
 
 script, setPrecipIntensity = argv
-weather = json.load(urllib2.urlopen("http://localhost:8080/?lat=-122.423&long=37.8267"))
+weather = json.load(urllib2.urlopen("http://ec2-184-72-208-140.compute-1.amazonaws.com:8080/?lat=-122.423&long=37.8267"))
 precipIntensity = weather['precipIntensity']
 
 if precipIntensity == (int(setPrecipIntensity)): 
