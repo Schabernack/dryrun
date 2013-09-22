5950dc70c3c25ac0022a2d3fa1cc3d0f52071e9b
Suhaim <suhaim@userevents.com>
2013-09-21 22:29:10 -0300

    updated to stop playin

--- play/play.py
+++ play/play.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 import pygame
+import time
+#pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load("myFile.wav")
+pygame.mixer.music.load("clouds.mp3")
 pygame.mixer.music.play()
-
+# sleep for the interval value when it is raining
+time.sleep(5)
+pygame.mixer.music.stop()
