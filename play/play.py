522eca3f88b2482340f5f210f2cc71db1a71f4de
Nicolas Neu <nicolas.m.neu@gmail.com>
2013-09-22 12:17:08 -0300

    play with rest

--- play/play.py
+++ play/play.py
@@ -2,10 +2,22 @@
 
 import pygame
 import time
-#pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load("clouds.mp3")
-pygame.mixer.music.play()
-# sleep for the interval value when it is raining
-time.sleep(5)
-pygame.mixer.music.stop()
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/startmusic')
+def start_music():
+    pygame.mixer.music.play()
+
+@app.route('/stopmusic')
+def stop_music():
+    pygame.mixer.music.stop()
+
+
+if __name__=='__main__':
+    app.run(debug = True, host = "0.0.0.0")
+    pygame.mixer.init()
+    pygame.mixer.music.load("clouds.mp3")
+
