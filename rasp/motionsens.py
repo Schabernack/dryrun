8a55939901aa367e0842149cd437e19f77d927b6
Nicolas Neu <nicolas.m.neu@gmail.com>
2013-09-22 14:07:28 -0300

    async

--- rasp/motionsens.py
+++ rasp/motionsens.py
@@ -4,6 +4,7 @@ from os import listdir
 from os.path import isfile, join
 import urllib2
 import requests
+from requests import async
 
 def get_filelist(mypath):
     onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
@@ -23,7 +24,7 @@ if __name__ == '__main__':
         if file_no > initial_no_of_files:
             print("Sent stopmusic command")
             #urllib2.urlopen(host + 'stopmusic')
-            s.get(host + 'stopmusic')
+            async.get(host + 'stopmusic')
             initial_no_of_files = file_no
             print('new initial: ' + str(initial_no_of_files))
 
